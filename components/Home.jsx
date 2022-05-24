0f423b0a637105e21e22dd42c1ca4b8da4a11473
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-05-24 23:21:43 +0500

    some fix

---
+++ components/Home.jsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+import { Header } from "./header/Header";
+import { Table2 } from "./table/Table";
+import { Navbar } from "./navbar/Navbar";
+import { Analytics } from "./analytics/Analytics";
+
+import styles from "../styles/home.module.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Lemonade Ape Stand</title>
+      </Head>
+      <Header />
+      <Navbar smDisplay={"smDisplay"} />
+      <Analytics />
+      <div className={"myTable"}>
+        <h1 className={styles.home__table_title}>
+          All transactions from launch till now
+        </h1>
+        <Table2 />
+      </div>
+      <div className={"margin"}>
+        <Navbar display="none" smNavbarDisplay={"smNavbarDisplay"} />
+      </div>
+    </div>
+  );
+};
+
