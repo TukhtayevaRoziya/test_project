574faeabe0ee0118c375c4785f8fe9e0247082a3
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-05-24 23:03:30 +0500

    delete extra file

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 
 import { Header } from "../components/header/Header";
-import { Table2 } from "./../components/table/Table";
-import { Navbar } from "./../components/navbar/Navbar";
-import { Analytics } from "./../components/analytics/Analytics";
+import { Table2 } from "../components/table/Table";
+import { Navbar } from "../components/navbar/Navbar";
+import { Analytics } from "../components/analytics/Analytics";
 
 import styles from '../styles/home.module.css'
 
