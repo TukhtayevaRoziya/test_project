4faba281e61587c6a865b052cb4ccd0c1f23820a
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-05-25 08:49:17 +0500

    some fix

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-
 import { Header } from "./header/Header";
 import { Table2 } from "./table/Table";
 import { Navbar } from "./navbar/Navbar";
@@ -13,8 +12,10 @@ export const Home = () => {
       <Head>
         <title>Lemonade Ape Stand</title>
       </Head>
+        <title>Lemonade Ape Stand</title>
       <Header />
       <Navbar smDisplay={"smDisplay"} />
+
       <Analytics />
       <div className={"myTable"}>
         <h1 className={styles.home__table_title}>
